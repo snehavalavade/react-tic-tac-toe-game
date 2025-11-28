9847d0620bd7204b57ade4fbaae069edd02efaf8
Sneha <151990521+snehavalavade@users.noreply.github.com>
2025-11-28 18:48:33 +0530

    Add files via upload

---
+++ components/Player.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export default function Player({
+    initialName,
+    symbol,
+    isActive,
+    onChangeName,
+}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
+    }
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let buttonCaption = "Edit";
+    if (isEditing) {
+        editablePlayerName = (
+            <input
+                type="text"
+                required
+                value={playerName}
+                onChange={handleChange}
+            />
+        );
+        // buttonCaption = "Save";
+    }
+    return (
+        <>
+            <li className={isActive ? "active" : undefined}>
+                <div>
+                    <span className="player">
+                        {editablePlayerName}
+                        <span className="player-symbol">{symbol}</span>
+                    </span>
+                    <button onClick={handleEditClick}>
+                        {isEditing ? "Save" : "Edit"}
+                    </button>
+                </div>
+            </li>
+        </>
+    );
+}
